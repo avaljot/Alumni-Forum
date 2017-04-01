8697e01471b9e459bc18ab3dd98a9fed9937f01d
CHARMEDALLOY\avalj <avaljotsingh@gmail.com>
2017-04-01 15:32:26 -0500

    First

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index');
+    res.render('index',{reg_user:req.session.user});
 });
 
 module.exports = router;
